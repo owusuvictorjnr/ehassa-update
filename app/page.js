174c7b4630a1ccd5d3ff4a61324ca6480175af3e
owusuvictorjnr <owusuvictor80@gmail.com>
2023-08-08 01:19:38 -0400

    use arrow function and change to HomePage

--- app/page.js
+++ app/page.js
@@ -1,5 +1,7 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  )
+import React from 'react'
+
+const HomePage = () => {
+  return <div>HomePage</div>
 }
+
+export default HomePage
