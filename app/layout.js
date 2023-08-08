20223e3634fe12199ae1dce1b903b3adc46b197c
owusuvictorjnr <owusuvictor80@gmail.com>
2023-08-07 23:26:00 -0400

    Initial Commit

--- app/layout.js
+++ app/layout.js
@@ -10,8 +10,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <div>
+        <nav></nav>
+        <main>{children}</main>
+        <footer></footer>
+      </div>
+    </>
   )
 }
